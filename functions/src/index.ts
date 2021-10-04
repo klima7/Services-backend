a6513d31afe7c831251a97c56de4e54f82a9c54c
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-10-04 17:27:42 +0200

    Google geocoding test added

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,27 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import * as geofirestore from "geofirestore";
+import {Client} from "@googlemaps/google-maps-services-js";
 
 admin.initializeApp();
 const firestore = admin.firestore();
 const GeoFirestore = geofirestore.initializeApp(firestore);
+const gmClient = new Client();
+
+
+export const testGeocoding = functions.https.onRequest((request, response) => {
+  return gmClient.geocode({
+    params: {
+      place_id: "ChIJd4k9VnDkG0cRgcHAll6lY6M",
+      key: "AIzaSyBgMDgU7VMT0L35f9TL4LZUB7v3NAS9pTs",
+    },
+  }).then((r) => {
+    response.send(r.data.results);
+  }, (f) => {
+    response.send(f);
+  });
+});
+
 
 export const testGeo = functions.https.onRequest((request, response) => {
   const geocollection = GeoFirestore.collection("restaurants");
