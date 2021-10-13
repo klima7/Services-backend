7658d08a4560302d6571f96cd44f0477b56939e0
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-10-13 17:31:54 +0200

    NamedLocation class added

--- functions/src/models.ts
+++ functions/src/models.ts
@@ -1,3 +1,5 @@
+import * as admin from "firebase-admin";
+
 export interface SetInfoParams {
   name: string | undefined;
   company: string | undefined;
@@ -14,4 +16,14 @@ export interface SetServicesParams {
 export interface SetWorkingAreaParams {
   placeId: string;
   radius: number;
+}
+
+export class NamedLocation {
+  coordinates: admin.firestore.GeoPoint;
+  name: string;
+
+  constructor(coordinates: admin.firestore.GeoPoint, name: string) {
+    this.coordinates = coordinates;
+    this.name = name;
+  }
 }
