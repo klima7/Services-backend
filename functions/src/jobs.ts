57f13168e341ec335aa6fd878094c22cf372fe06
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-10-29 22:17:45 +0200

    getAvailableIds function renamed

--- functions/src/jobs.ts
+++ functions/src/jobs.ts
@@ -4,7 +4,7 @@ import * as admin from "firebase-admin";
 const firestore = admin.firestore();
 
 
-export const getAvailableIds = functions.https.onCall(async (data, context) => {
+export const getNewIds = functions.https.onCall(async (data, context) => {
   const res = await firestore.collection("jobs").listDocuments();
   const ids = res.map((it) => it.id);
   return ids;
