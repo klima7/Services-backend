4172bc8d23ec1782e64e3d7092f362767e2f7800
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-11-29 19:13:53 +0100

    clients-setInfo cleaned up

--- functions/src/services/clients.ts
+++ functions/src/services/clients.ts
@@ -1,6 +1,7 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import {Client} from "../interfaces/firestore";
+import * as Joi from "joi";
 
 const firestore = admin.firestore();
 
@@ -26,21 +27,44 @@ export const createAccount = functions.https.onCall(async (_data, context) => {
 
 export const deleteAccount = functions.https.onCall((_data, _context) => {
   console.log("Deleting account");
+  // TODO: implement
 });
 
 
-export interface SetClientInfoParams {
-  name: string | undefined;
-  phone: string | undefined;
+abstract class SetInfoParams {
+  abstract name: string | null;
+  abstract phone: string | null;
+
+  static schema = Joi.object({
+    name: Joi.string().required().min(1).max(40),
+    phone: Joi.string().allow(null).length(9),
+  });
 }
 
-export const setInfo = functions.https.onCall((data, context) => {
+export const setInfo = functions.https.onCall(async (data, context) => {
   const uid = context.auth?.uid;
-  const info: SetClientInfoParams = data; // TODO: Validate
   if (uid == undefined) {
-    throw new functions.https.HttpsError("internal", "Users is not authenticated");
+    throw new functions.https.HttpsError("unauthenticated", "User is not authenticated");
   }
-  return firestore.collection("clients").doc(uid).update({
-    info: info,
-  });
+
+  const params: SetInfoParams = data;
+  const validation = SetInfoParams.schema.validate(params);
+  if (validation.error) {
+    throw new functions.https.HttpsError("invalid-argument", "Invalid parameters passed");
+  }
+
+  // Check whether client has an account
+  const client = (await firestore.collection("clients").doc(uid).get()).data() as Client;
+  if (client == null) {
+    throw new functions.https.HttpsError("failed-precondition", "User does not have account");
+  }
+
+  const clientData: Partial<Client> = {
+    info: {
+      name: params.name,
+      phone: params.phone,
+    },
+  };
+
+  await firestore.collection("clients").doc(uid).update(clientData);
 });
