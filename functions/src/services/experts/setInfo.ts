30ea165aaadce8290776d72f570ec2196bffc44a
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-11-29 21:12:55 +0100

    expert-setInfo improved

--- functions/src/services/experts/setInfo.ts
+++ functions/src/services/experts/setInfo.ts
@@ -1,26 +1,59 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import * as Joi from "joi";
+import {ExpertUpdate} from "../../interfaces/firestore";
 
 const firestore = admin.firestore();
 
 
 interface SetInfoParams {
-  name: string | undefined;
-  company: string | undefined;
-  description: string | undefined;
-  email: string | undefined;
-  phone: string | undefined;
-  website: string | undefined;
+  name: string | null;
+  company: string | null;
+  description: string | null;
+  email: string | null;
+  phone: string | null;
+  website: string | null;
 }
 
 
-export const setInfo = functions.https.onCall((data, context) => {
+const schemaSetInfoParams = Joi.object({
+  name: Joi.string().required().min(1).max(40),
+  company: Joi.string().allow(null).min(1).max(40),
+  description: Joi.string().allow(null).min(1).max(500),
+  email: Joi.string().allow(null).min(1).max(50),
+  phone: Joi.string().allow(null).length(9),
+  website: Joi.string().allow(null).min(1).max(200),
+});
+
+
+export const setInfo = functions.https.onCall(async (data, context) => {
   const uid = context.auth?.uid;
-  const info: SetInfoParams = data; // TODO: Validate
   if (uid == undefined) {
-    throw new functions.https.HttpsError("internal", "Users is not authenticated");
+    throw new functions.https.HttpsError("unauthenticated", "User is not authenticated");
   }
-  return firestore.collection("experts").doc(uid).update({
-    info: info,
-  });
+
+  const params: SetInfoParams = data;
+  const validation = schemaSetInfoParams.validate(params);
+  if (validation.error) {
+    throw new functions.https.HttpsError("invalid-argument", "Invalid parameters passed");
+  }
+
+  // Check whether expert has an account
+  const expert = (await firestore.collection("experts").doc(uid).get()).data() as ExpertUpdate;
+  if (expert == null) {
+    throw new functions.https.HttpsError("failed-precondition", "User does not have expert account");
+  }
+
+  const expertData: Partial<ExpertUpdate> = {
+    info: {
+      company: params.company,
+      description: params.description,
+      email: params.email,
+      name: params.name,
+      phone: params.phone,
+      website: params.website,
+    },
+  };
+
+  await firestore.collection("clients").doc(uid).update(expertData);
 });
