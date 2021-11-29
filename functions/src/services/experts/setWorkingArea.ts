7ba49c245a0b9ea8f3bc0319f938e7a2c39e9bb7
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-11-29 22:22:43 +0100

    expert-setWorkingArea improved

--- functions/src/services/experts/setWorkingArea.ts
+++ functions/src/services/experts/setWorkingArea.ts
@@ -1,6 +1,8 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import * as Joi from "joi";
 import {geocodingRepository} from "../../utils/geocoding";
+import {ExpertUpdate} from "../../interfaces/firestore";
 
 const firestore = admin.firestore();
 
@@ -10,30 +12,44 @@ interface SetWorkingAreaParams {
   radius: number;
 }
 
+const schemaSetWorkingAreaParams = Joi.object({
+  placeId: Joi.string().required().min(1).max(100),
+  radius: Joi.number().required().min(0).max(50),
+});
+
 
 export const setWorkingArea = functions.https.onCall(async (data, context) => {
   const uid = context.auth?.uid;
-  const params: SetWorkingAreaParams = data; // TODO: Validate
   if (uid == undefined) {
-    throw new functions.https.HttpsError("internal", "Users is not authenticated");
-  }
-  console.log(`expert-setWorkingArea(${params.placeId}, ${params.radius})`);
-
-  const gcResult = await geocodingRepository.getLocationByPlaceId(params.placeId);
-  console.log("Geocoded location: " + JSON.stringify(gcResult));
-
-  if (gcResult.country != "PL") {
-    throw new functions.https.HttpsError("internal", "Provided location is not in Poland");
+    throw new functions.https.HttpsError("unauthenticated", "User is not authenticated");
   }
 
-  await firestore.collection("experts").doc(uid).update({
+  // Check whether user have expert account
+  const expert = (await firestore.collection("experts").doc(uid).get()).data() as ExpertUpdate;
+  if (expert == null) {
+    throw new functions.https.HttpsError("failed-precondition", "User does not have expert account");
+  }
+
+  const params: SetWorkingAreaParams = data;
+  const validation = schemaSetWorkingAreaParams.validate(params);
+  if (validation.error) {
+    throw new functions.https.HttpsError("invalid-argument", "Invalid parameters passed");
+  }
+
+  const geocodingResult = await geocodingRepository.getLocationByPlaceId(params.placeId);
+
+  if (geocodingResult.country != "PL") {
+    throw new functions.https.HttpsError("invalid-argument", "Provided location is not in Poland");
+  }
+
+  const expertData: Partial<ExpertUpdate> = {
     workingArea: {
-      coordinates: new admin.firestore.GeoPoint(gcResult.latitude, gcResult.longitude),
-      locationName: gcResult.name,
+      coordinates: new admin.firestore.GeoPoint(geocodingResult.latitude, geocodingResult.longitude),
+      locationName: geocodingResult.name,
       locationId: params.placeId,
       radius: params.radius,
     },
-  });
+  };
 
-  console.log("Working area in firestore updated");
+  await firestore.collection("experts").doc(uid).update(expertData);
 });
