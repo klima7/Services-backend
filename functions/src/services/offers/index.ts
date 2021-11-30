4712e7cf9b7ed97b58021ce8dffa0fbf51aae3ef
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-11-30 11:48:40 +0100

    offers-setStatus implemented

--- functions/src/services/offers/index.ts
+++ functions/src/services/offers/index.ts
@@ -1,1 +1,2 @@
 export * from "./setArchived";
+export * from "./setStatus";
---
+++ functions/src/services/offers/setStatus.ts
@@ -0,0 +1,44 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import * as Joi from "joi";
+import {Offer} from "../../interfaces/firestore";
+
+const firestore = admin.firestore();
+
+
+export interface SetStatusParams {
+  offerId: string;
+  status: number;
+}
+
+
+const schemaSetStatusParams = Joi.object({
+  offerId: Joi.string().required().max(100),
+  status: Joi.number().required().allow(0, 1, 2, 3),
+});
+
+
+export const setStatus = functions.https.onCall(async (data, context) => {
+  const uid = context.auth?.uid;
+  if (uid == undefined) {
+    throw new functions.https.HttpsError("unauthenticated", "User is not authenticated");
+  }
+
+  const params: SetStatusParams = data;
+  const validation = schemaSetStatusParams.validate(params);
+  if (validation.error) {
+    throw new functions.https.HttpsError("invalid-argument", "Invalid parameters passed");
+  }
+
+  // Verify wheter caller is offer client of expert
+  const offer = (await firestore.collection("offers").doc(params.offerId).get()).data() as Offer;
+  if (uid != offer.clientId && uid != offer.expertId) {
+    throw new functions.https.HttpsError("invalid-argument", "Requested offer does not belong to requesting user");
+  }
+
+  const offerData: Partial<Offer> = {
+    status: params.status,
+  };
+
+  await firestore.collection("offers").doc(params.offerId).update(offerData);
+});
