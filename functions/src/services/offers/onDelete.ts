0a21a5745aa7beefd06d9dcc45167ae11ff6205b
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-12-12 21:17:01 +0100

    Deleting chat images

--- functions/src/services/offers/onDelete.ts
+++ functions/src/services/offers/onDelete.ts
@@ -4,29 +4,37 @@ import {JobUpdate, Offer} from "../../interfaces/firestore";
 
 
 const firestore = admin.firestore();
+const bucket = admin.storage().bucket();
 
 
 export const onDelete = functions.firestore.document("offers/{offerId}").onDelete(async (snapshot, context) => {
   const offer = snapshot.data() as Offer;
   const offerId = context.params.offerId;
-  const jobId = offer.jobId;
 
-  // Update job
-  const jobUpdate: Partial<JobUpdate> = {
-    offers: admin.firestore.FieldValue.arrayRemove(offerId),
-    unreadOffers: admin.firestore.FieldValue.arrayRemove(offerId),
-  };
-  firestore.collection("jobs").doc(jobId).update(jobUpdate);
-
-  // Delete messages
+  await updateJob(offerId, offer);
+  await deleteImages(offerId);
   await deleteMessages(offerId);
 });
 
 
-async function deleteMessages(offerId: string) {
-  const messages = firestore.collection("offers").doc(offerId).collection("messages").get();
-  (await messages).forEach(async (messageDoc) => {
-    await messageDoc.ref.delete();
-  });
+async function updateJob(offerId: string, offer: Offer) {
+  const jobUpdate: Partial<JobUpdate> = {
+    offers: admin.firestore.FieldValue.arrayRemove(offerId),
+    unreadOffers: admin.firestore.FieldValue.arrayRemove(offerId),
+  };
+  firestore.collection("jobs").doc(offer.jobId).update(jobUpdate);
 }
 
+
+async function deleteMessages(offerId: string) {
+  const messages = firestore.collection("offers").doc(offerId).collection("messages").get();
+  (await messages).forEach(async (messageDoc) => await messageDoc.ref.delete());
+}
+
+
+async function deleteImages(offerId: string) {
+  const path = `chats_images/${offerId}`;
+  await bucket.deleteFiles({
+    prefix: path,
+  });
+}
