13d9370137b4f56b2e0f47cb7ba4ec1a296597f7
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-12-12 21:55:35 +0100

    Deleting user if no account

--- functions/src/services/clients/deleteAccount.ts
+++ functions/src/services/clients/deleteAccount.ts
@@ -3,15 +3,20 @@ import * as admin from "firebase-admin";
 
 
 const firestore = admin.firestore();
+const auth = admin.auth();
 
 
 export const deleteAccount = functions.https.onCall(async (_data, context) => {
-  console.log("Deleting account");
-
+  // Delete client
   const uid = context.auth?.uid;
   if (uid == undefined) {
     throw new functions.https.HttpsError("unauthenticated", "User is not authenticated");
   }
-
   await firestore.collection("clients").doc(uid).delete();
+
+  // Delete user if no accounts
+  const expert = await firestore.collection("experts").doc(uid).get();
+  if (!expert.exists) {
+    await auth.deleteUser(uid);
+  }
 });
