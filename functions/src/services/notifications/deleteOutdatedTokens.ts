720b1b1ab40c23341e64a894e7f5c16c3a76c772
klima7 <ukasz.klimkiewicz2@wp.pl>
2022-01-04 11:48:31 +0100

    Deleting tokens every two months

--- functions/src/services/notifications/deleteOutdatedTokens.ts
+++ functions/src/services/notifications/deleteOutdatedTokens.ts
@@ -2,7 +2,7 @@ import * as functions from "firebase-functions";
 import {deleteOutdatedTokens} from "../../lib/tokens";
 
 
-export const periodicallyDeleteOutdatedTokens = functions.pubsub.schedule("0 0 * * *")
+export const periodicallyDeleteOutdatedTokens = functions.pubsub.schedule("0 0 01 */2 *")
     .timeZone("Europe/Warsaw")
     .onRun(async (_context) => {
       await deleteOutdatedTokens();
